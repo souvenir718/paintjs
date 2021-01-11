e6add221c4ac2154e9a5b8f9c6f5a543c1b8654d
souvenir <mongkey0@naver.com>
2021-01-11 22:11:42 +0900

    save image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 700;
@@ -13,6 +14,9 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
 //선의 색, 두께
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, "white", "white");
 ctx.strokeStyle= INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -96,12 +100,29 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    // canvasAPI - toDataURL()
+    const image = canvas.toDataURL();
+    // console.log(image);
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "From PaintJS";
+    // download는 a태그의 attribute
+    // console.log(link);
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM)
 }
 
 // console.log(Array.from(colors));
@@ -114,4 +135,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
