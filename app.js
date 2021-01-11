ecd716ce87156c07162e9176127a8625b6fee07f
souvenir <mongkey0@naver.com>
2021-01-11 21:47:35 +0900

    change brush size

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
-
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 // canvas에는 pixel modifer에 사이즈를 줘야 한다.
 // pixel을 다루는 윈도우에 크기를 알려줘야 한다.
@@ -16,6 +17,8 @@ ctx.lineWidth = 2.5;
 let painting = false;
 // clientX, clientY : 전체 창 위의 좌표
 // offsetX, offsetY : canvas 위의 좌표
+
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -63,6 +66,23 @@ function handleColorClick(event) {
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+    // console.log(event.target.value);
+    const size = event.target.value;
+
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint"
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -73,3 +93,11 @@ if(canvas){
 // console.log(Array.from(colors));
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
